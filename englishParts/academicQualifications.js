b4498456790be929e4cd55cbc6f9fe8e67bd0b55
Itsuki KUBOI <emacs75e@gmail.com>
2022-09-25 23:50:08 +0900

    mod

--- englishParts/academicQualifications.js
+++ englishParts/academicQualifications.js
@@ -4,45 +4,55 @@ document.write(`
 
       <li>2021.3.23
       <br><b>Master of Engineering</b>, University of Fukui
+      <br> - Thesis Title : <a href="https://www.eng.u-fukui.ac.jp/wp-content/uploads/MastarThesis2020.html" target="_blank">Study on L-curve and LSMR Method on Deconvolution Problem</a>
+      <br> - (LSMR法を利用したデコンボリューション問題の解法におけるL-curveの検討)
+      <br> - Supervisor : Professor Yohsuke HOSODA and etc.
+      <br> - GPA : 4.00 / 4.00
       </li>
 
       <br>
       <li>2020.3.23
       <br><b>Minor of Management of Technology</b>, University of Fukui
+      <br> - Studied management lectures and went to study abroad
+      <br> - Assigned to project on regional revitalization funded by Fukui Prefecture
+      <br> - Supervisor : Professor Takuji TAKEMOTO, Toshihiro INOUE and Sachiyo KAWAKAMI
       </li>
 
+      <br>
       <li>2019.4.1 - 2021.3.23
-      <br><b>Master's Course Student</b>
+      <br>Master's Course Student
       <br>Information Science Course, Graduate School of Engineering, University of Fukui
-      <br> - GPA : 4.00/4.00
-      <br> - Management of Technology Course Finished (Its minor certificated in 2020.3)
+      
       </li>
 
       <br>
       <li>2019.3.22
       <br><b>Bachelor of Engineering</b>, University of Fukui
+      <br> - Thesis Title : Numerical Computation of Steady Iteration on Deconvolution Problem
+      <br> - (定常反復を用いたデコンボリューション問題の数値計算について)
+      <br> - Supervisor : Professor Yohsuke HOSODA
+      <br> - GPA : 3.18 / 4.00
       </li>
 
       <br>
       <li>2017.4.1 - 2019.3.22
-      <br><b>Bachelors Course Student</b>
+      <br>Bachelors Course Student
       <br>Information Science Course, School of Engineering, University of Fukui
-      <br> - GPA : 3.18/4.00
-      <br> - Teacher Training Course Finished (Its license acquired in 2019.3)
+      <br> - Teacher Training Course Finished (Its license acquired in 2019.3.31)
       </li>
 
       <br>
       <li>2015.4.1 - 2017.3.31
-      <br><b>Bachelors Course Student</b>
+      <br>Bachelors Course Student
       <br>Applied Mathematics and Informatics Course, Faculty of Science and Engineering, Ryukoku University
       </li>
 
       <br>
       <li>2012.4.1 - 2015.3.31
-      <br><b>Student</b>
+      <br>Student
       <br>Shiga Prefectural Torahime High School
-      <br> - <b>American Football Team</b>
-      <br> - Kyoto-Shiga-Tokai national team, LT/DT, <a href="http://kantokoukou-football.com/cms/index.php?page_id=27" target="_blank">NEW YEAR BOWL III(2014)</a> at Amino Vital Field, Tokyo
+      <br> - <b>American Football Team
+      <br> - <b>Kyoto-Shiga-Tokai national team</b>, LT/DT, <a href="http://kantokoukou-football.com/cms/index.php?page_id=27" target="_blank">NEW YEAR BOWL III(2014)</a> at Amino Vital Field, Tokyo
       </li>
 
       </ul>
