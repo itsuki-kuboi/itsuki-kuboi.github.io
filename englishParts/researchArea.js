b9c4d7da75e229c969d0d23388d6eb32eb3c180b
Itsuki KUBOI <emacs75e@gmail.com>
2022-09-26 00:47:13 +0900

    mod

--- englishParts/researchArea.js
+++ englishParts/researchArea.js
@@ -2,8 +2,15 @@ document.write(`
 <h2>Research Area</h2>
 <ul>
   <li><b>Major : Numerical Analysis</b>
-    <br>Inverse Problem : Image Deblur Problem, Large Scaled and Ill-conditioned Problem
-    <br>Linear Solution Method : Linear Equation, Regularization, Singular Value Decomposition
+    <br>Inverse Problem
+    <br> - Large Scaled and Ill-conditioned Problem
+    <br> - Image Deblur Problem
+    <br>Linear Method
+    <br> - Singular Value Decomposition : bisection method, DQDS method
+    <br> - Linear Direct Solution Method : LU decomposition, SVD
+    <br> - Linear Iterative Solution Method : Kaczmarz method, Jacobi method, Krylov subspace method(GMRESs, LSQR, LSMR, etc.)
+    <br>Regularization
+    <br> - L-curve method, condition L-curve method
     <br><i>Related Area : DataScience, BigData Analysis, Optimization</i>
   </li>
 
