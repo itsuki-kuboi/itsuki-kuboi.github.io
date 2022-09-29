fa4170c47859f8a3b8082ff21d9f5db1245c4164
Itsuki KUBOI <emacs75e@gmail.com>
2022-09-30 03:20:55 +0900

    <br>

--- englishParts/press.js
+++ englishParts/press.js
@@ -8,12 +8,14 @@ document.write(`
   <br> - This short video is pressed on YouTube, Twitter, Instagram and Facebook.
   </li>
 
+  <br>
   <li>
   2021.4.20
   <br><a href="https://www.u-fukui-kogyokai.com/pdfs/recent-conditions-2021-02.pdf" target="_blank">Recent Report on FY2020</a>
   <br> - Pressed by Alumni Association of Engineering, University of Fukui, Japan
   </li>
 
+  <br>
   <li>
   2015.1.4
   <br><a href="http://www.city.takashima.lg.jp/www/contents/1420701887416/index.html" target="_blank">Mayor's WEB Office Dairy</a>
