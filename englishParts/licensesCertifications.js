6c3865b62134916b9e34c8dcab35a262f67b23d2
Itsuki KUBOI <emacs75e@gmail.com>
2022-10-03 00:48:20 +0900

    mod

--- englishParts/licensesCertifications.js
+++ englishParts/licensesCertifications.js
@@ -8,20 +8,20 @@ document.write(`
 
   <br>
   <li>2019.3.31
-  <br><b>Fundamental Information Technology Engineer Examination</b>
-  <br>IPA: Information-technology Promotion Agency, Japan
+  <br><b>High school teaching Certificate for Mathematics</b>
+  <br>Shiga Prefectural Board of Education, Shiga, Japan
   </li>
   
   <br>
   <li>2021.8.16
   <br><b>Information Technology Passport Examination</b>
-  <br>IPA: Information-technology Promotion Agency, Japan
+  <br>IPA, Japan
   </li>
   
   <br>
   <li>2021.12.27
-  <br><b>High school teaching Certificate for Mathematics</b>
-  <br>Shiga Prefectural Board of Education, Shiga, Japan
+  <br><b>Fundamental Information Technology Engineer Examination</b>
+  <br>IPA, Japan
   </li>
 </ul>
 `);
