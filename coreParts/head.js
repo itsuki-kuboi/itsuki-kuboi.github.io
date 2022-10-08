6a3bd9923d7013eae90f37d3b74a8ca708f1152e
Itsuki KUBOI <emacs75e@gmail.com>
2022-10-08 15:44:19 +0900

    mod

--- coreParts/head.js
+++ coreParts/head.js
@@ -6,7 +6,7 @@ document.write(`
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="Content-Style-Type" content="text/css">
 <!--<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">-->
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
+<meta name="viewport" content="width=device-width, initial-scale=0.6, minimum-scale=1, user-scalable=yes">
 
 <link rel="stylesheet" href="./css/cv.css">
 <title>Itsuki KUBOI's  Website</title>
