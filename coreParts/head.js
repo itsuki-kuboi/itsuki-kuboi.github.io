186594908fc189b30bf133976baa408a6ace99b4
Itsuki KUBOI <emacs75e@gmail.com>
2022-10-10 22:54:48 +0900

    mod

--- coreParts/head.js
+++ coreParts/head.js
@@ -2,8 +2,10 @@
 /*https://www.site-convert.com/archives/1528#outline__1*/
 /*https://www.webdesignleaves.com/wp/htmlcss/450/*/
 
+/*charsetは各HTMLで宣言しないと反映されない*/
+
 document.write(`
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Type" content="text/html">
 <meta http-equiv="Content-Style-Type" content="text/css">
 <!--<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">-->
 <meta name="viewport" content="width=device-width, initial-scale=0.6, minimum-scale=0.6, user-scalable=yes">
