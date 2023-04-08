2852720fa6f8cbd26d7a5099cdfbeef56a01c137
Itsuki KUBOI <emacs75e@gmail.com>
2023-04-09 00:55:50 +0900

    mod

---
+++ englishParts/contact.js
@@ -0,0 +1,11 @@
+document.write(`
+<h2>Contacts</h2>
+<p>Itsuki KUBOI(久保井 五貴)</p>
+<p>
+	Facebook:
+	<a href="http://www.facebook.com/itsuki.kuboi" target="_blank">Click here</a>
+	<br>
+	LinkedIn:
+	<a href="http://www.linkedin.com/in/itsuki-kuboi" target="_blank">Click here</a>
+</p>
+`)
---
+++ japaneseParts/contact.js
@@ -0,0 +1,11 @@
+document.write(`
+<h2>連絡先</h2>
+<p>久保井 五貴</p>
+<p>
+	Facebook:
+	<a href="http://www.facebook.com/itsuki.kuboi" target="_blank">Click here</a>
+	<br>
+	LinkedIn:
+	<a href="http://www.linkedin.com/in/itsuki-kuboi" target="_blank">Click here</a>
+</p>
+`)
