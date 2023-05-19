4c9ff47edd67cda667b0d2aa3a6f7414b11b9a3c
Itsuki KUBOI <emacs75e@gmail.com>
2023-05-19 21:44:04 +0900

    mod

--- englishParts/publication.js
+++ englishParts/publication.js
@@ -19,21 +19,14 @@ document.write(`
 
       
       <li>Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata, Kinji Kimura and Yoshimasa Nakamura,
-        <br><a href="https://www.american-cse.org/static/CSCE22-book-abstracts-printing.pdf" target="_blank">
-        "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
-        <br>Proceedings of The 28th International Conference on Parallel & Distributed Processing Techniques & Applications,
-        <br>American Council on Science and Education,
-        <br>Luxor Hotel, Las Vegas, USA,
-        <br>2022.7.25
-        <br><i>and to be published as</i>
         <br>"On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method",
         <br><a href="https://link.springer.com/book/10.1007/978-3-030-69984-0" target="_blank">
         Advances in Parallel & Distributed Processing, and Applications</a>,
+        <br>Proceedings from The 28th International Conference on Parallel & Distributed Processing Techniques & Applications and others,
         <br>Springer,
         <br>Switzerland AG.,
         <br>2023.x.x
       </li>
-
     </ol>
   </li>
 
@@ -46,24 +39,37 @@ document.write(`
       
       <!-- <br> -->
       <li>
+        Miho Chiyonobu, Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata, Kinji Kimura and Yoshimasa Nakamura,
+              <br>(千代延 未帆, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正),
+        <br>"Proposal on how to reduce the number of data when implementing deep learning of stop conditions for the LSMR method",
+              <br>(<a href="http://id.nii.ac.jp/1001/00225107/" target="_blank">
+              LSMR法の停止条件を深層学習する際のデータ数削減方法に関する提案</a>),
+        <br>The Special Interest Group Technical Reports of IPSJ, MPS142,
+        <br>MPS, IPSJ,
+        <br>Tokyo, Japan,
+        <br>2023.3.2
+      </li>
+
+      <br>
+      <li>
         Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata, Kinji Kimura and Yoshimasa Nakamura,
-        <br>(久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正),
+              <br>(久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正),
         <br>"On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method",
-              <br>(<a href="https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=218930&item_no=1&page_id=13&block_id=8" target="_blank">
+              <br>(<a href="http://id.nii.ac.jp/1001/00218822/" target="_blank">
               不良条件問題を解くためのLSMR法におけるDQDS法を用いたcondition L-curveの高速計算について</a>),
         <br>The Special Interest Group Technical Reports of IPSJ, MPS139,
         <br>MPS, IPSJ,
         <br>Tokyo, Japan,
-        <br>2022.7.25
+        <br>2022.7.19
       </li>
 
       <br>
       <li>
         Itsuki Kuboi, Yohsuke Hosoda and Shin-ya Ozawa,
-        <br>(久保井五貴, 細田陽介, 小澤伸也),
+              <br>(久保井五貴, 細田陽介, 小澤伸也),
         <br>"On a Numerical Computation on Deconvolution Problem using Steady Iteration",
-        <br>(<a href="http://2019.jhes.jp/files/program.pdf" target="_blank">
-        定常反復を用いたデコンボリューション問題の数値計算について</a>),
+              <br>(<a href="http://2019.jhes.jp/files/program.pdf" target="_blank">
+              定常反復を用いたデコンボリューション問題の数値計算について</a>),
         <br>Joint conference of Hokuriku chapters of Electrical and information Societies 2019,
         <br>Cosponsor[IEEJ, IEICE, ITE, JSMBE, IPSJ, SICE, ASJ, IEIEJ Hokuriku Sections and IEEE Nagoya Section],
         <br>National Institute of Technology, Ishikawa College, Ishikawa, Japan,
@@ -80,11 +86,25 @@ document.write(`
       <!-- Insert -->
       
       <!-- <br> -->
+
+      <li>
+        Miho Chiyonobu*, Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata, Kinji Kimura and Yoshimasa Nakamura,
+              <br>(千代延 未帆*, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正),
+        <br>"Proposal on how to reduce the number of data when implementing deep learning of stop conditions for the LSMR method",
+        <br>(<a href="https://www.ipsj.or.jp/kenkyukai/event/mps142bio73.html" target="_blank">
+              LSMR法の停止条件を深層学習する際のデータ数削減方法に関する提案</a>),
+        <br>MPS142,
+        <br>MPS, IPSJ,
+        <br>JAIST, Ishikawa, Japan,
+        <br>2023.3.10
+      </li>
+
+    <br>
       <li>
         Rika Tanaka*, Itsuki Kuboi, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata, Kinji Kimura and Yoshimasa Nakamura,
-        <br>(田中利佳*, 久保井五貴, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正),
+              <br>(田中利佳*, 久保井五貴, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正),
         <br>"On a Machine Learning and Stopping Iteration using Condition Number on LSMR Method"</a>,
-        <br>(LSMR法における条件数を用いた反復の打ち切りと機械学習について),
+              <br>(LSMR法における条件数を用いた反復の打ち切りと機械学習について),
         <br>The 20th Computational Mathematics  Workshop,
         <br>(第20回計算数学研究会),
         <br><a href="https://www-is.amp.i.kyoto-u.ac.jp/lab/cmc/" target="_blank">Applied Mathematical Analysis Laboratory</a>, Kyoto University,
@@ -96,7 +116,7 @@ document.write(`
       <li>
         Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata*, Kinji Kimura and Yoshimasa Nakamura,
         <br><a href="https://www.american-cse.org/static/CSCE22-book-abstracts-printing.pdf" target="_blank">
-          "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
+        "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
         <br>The 28th International Conference on Parallel & Distributed Processing Techniques & Applications,
         <!-- <br> - The 2022 World Congress in Computer Science, Computer Engineering, and Applied Computing, -->
         <br>American Council on Science and Education,
@@ -107,10 +127,10 @@ document.write(`
       <br>
       <li>
         Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata*, Kinji Kimura and Yoshimasa Nakamura,
-        <br>(久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美*, 木村欣司, 中村佳正),
+              <br>(久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美*, 木村欣司, 中村佳正),
         <br>"On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method",
         <br>(<a href="https://www.ipsj.or.jp/kenkyukai/event/mps139.html" target="_blank">
-        不良条件問題を解くためのLSMR法におけるDQDS法を用いたcondition L-curveの高速計算について</a>),
+              不良条件問題を解くためのLSMR法におけるDQDS法を用いたcondition L-curveの高速計算について</a>),
         <br>MPS139,
         <br>MPS, IPSJ,
         <br>Luxor Hotel, Las Vegas, USA,
@@ -120,10 +140,10 @@ document.write(`
       <br>
       <li>
         Kinji Kimura*, Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda and Masami Takata,
-        <br>(木村欣司*, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美), 
+              <br>(木村欣司*, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美), 
         <br>"Discrete Toda Molecule Equation and Inverse Problem",
         <br>(<a href="http://air.s.kanazawa-u.ac.jp/~ohara/risacon21.html" target="_blank">
-        離散戸田分子方程式と逆問題</a>),
+              離散戸田分子方程式と逆問題</a>),
         <br>Risa/Asir Conference 2021,
         <br>Kanazawa University, Ishikawa , Japan,
         <br>Online, Japan,
@@ -133,10 +153,10 @@ document.write(`
       <br>
       <li>
         Itsuki Kuboi*, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata and Kinji Kimura,
-        <br>(久保井五貴*, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司), 
+              <br>(久保井五貴*, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司), 
         <br>"Study on Condition L-curve on Deconvolution Problem",
         <br>(<a href="http://union2021.jsiam.org/program#4863" target="_blank">
-        デコンボリューション問題におけるcondition L-curveの検討</a>),
+              デコンボリューション問題におけるcondition L-curveの検討</a>),
         <br>Joint Meeting of JSIAM Activity Groups 2019,
         <br>JSIAM,
         <br>Online, Japan,
@@ -146,10 +166,10 @@ document.write(`
       <br>
       <li>
         Itsuki Kuboi*, Yohsuke Hosoda and Shin-ya Ozawa,
-        <br>(久保井五貴*, 細田陽介, 小澤伸也),
+              <br>(久保井五貴*, 細田陽介, 小澤伸也),
         <br>"On a Numerical Computation on Deconvolution Problem using Steady Iteration",
         <br>(<a href="http://2019.jhes.jp/files/program.pdf" target="_blank">
-        定常反復を用いたデコンボリューション問題の数値計算について</a>),
+              定常反復を用いたデコンボリューション問題の数値計算について</a>),
         <br>Joint conference of Hokuriku chapters of Electrical and information Societies 2019,
         <br>Cosponsor[IEEJ, IEICE, ITE, JSMBE, IPSJ, SICE, ASJ, IEIEJs, IEEE Hokuriku Sections and IEEE Nagoya Section],
         <br>National Institute of Technology, Ishikawa College, Ishikawa, Japan,
@@ -158,6 +178,7 @@ document.write(`
     </ol>
   </li>
 
+  
   <br>
   <li><b>Notation</b>
     <ol reversed>
--- japaneseParts/publication.js
+++ japaneseParts/publication.js
@@ -19,16 +19,10 @@ document.write(`
 
       
       <li>Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata, Kinji Kimura and Yoshimasa Nakamura,
-        <br><a href="https://www.american-cse.org/static/CSCE22-book-abstracts-printing.pdf" target="_blank">
-        "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
-        <br>Proceedings of The 28th International Conference on Parallel & Distributed Processing Techniques & Applications,
-        <br>American Council on Science and Education,
-        <br>Luxor Hotel, Las Vegas, USA,
-        <br>2022.7.25
-        <br><i>and to be published as</i>
         <br>"On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method",
         <br><a href="https://link.springer.com/book/10.1007/978-3-030-69984-0" target="_blank">
         Advances in Parallel & Distributed Processing, and Applications</a>,
+        <br>Proceedings from The 28th International Conference on Parallel & Distributed Processing Techniques & Applications and others,
         <br>Springer,
         <br>Switzerland AG.,
         <br>2023.x.x
@@ -46,19 +40,31 @@ document.write(`
       
       <!-- <br> -->
       <li>
+        千代延 未帆, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正, 
+        <br><a href="http://id.nii.ac.jp/1001/00225107/" target="_blank">
+              "LSMR法の停止条件を深層学習する際のデータ数削減方法に関する提案"</a>,
+        <br>研究報告, MPS142,
+        <br>MPS研究会, 情報処理学会,
+        <br>東京, 日本,
+        <br>2023.3.2
+      </li>
+
+      <br>
+      <li>
         久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正, 
-        <br><a href="https://ipsj.ixsq.nii.ac.jp/ej/?action=pages_view_main&active_action=repository_view_main_item_detail&item_id=218930&item_no=1&page_id=13&block_id=8" target="_blank">
-              "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
+        <br><a href="http://id.nii.ac.jp/1001/00218822/" target="_blank">
+              "不良条件問題を解くためのLSMR法におけるDQDS法を用いたcondition L-curveの高速計算について"</a>,
         <br>研究報告, MPS139,
         <br>MPS研究会, 情報処理学会,
         <br>東京, 日本,
-        <br>2022.7.25
+        <br>2022.7.19
       </li>
 
       <br>
       <li>
         久保井五貴, 細田陽介, 小澤伸也,
-        <br><a href="http://2019.jhes.jp/files/program.pdf" target="_blank">"定常反復を用いたデコンボリューション問題の数値計算について"</a>,
+        <br><a href="http://2019.jhes.jp/files/program.pdf" target="_blank">
+              "定常反復を用いたデコンボリューション問題の数値計算について"</a>,
         <br>2019年度電気・情報関係学会北陸支部連合大会,
         <br>共催[IEEJ, IEICE, ITE, JSMBE, IPSJ, SICE, ASJ, IEIEJ各北陸支部, IEEE名古屋支部],
         <br>石川工業高等専門学校, 石川, 日本,
@@ -76,10 +82,22 @@ document.write(`
       
       <!-- <br> -->
       <li>
+        千代延 未帆*, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正,
+        <br><a href="https://www.ipsj.or.jp/kenkyukai/event/mps142bio73.html" target="_blank">
+              "LSMR法の停止条件を深層学習する際のデータ数削減方法に関する提案"</a>,
+        <br>MPS142,
+        <br>MPS研究会, 情報処理学会,
+        <br>北陸先端技術大学院大学, 石川, 日本,
+        <br>2023.3.10
+      </li>
+
+      <br>
+      <li>
         田中利佳*, 久保井五貴, 小澤伸也, 細田陽介, 高田雅美, 木村欣司, 中村佳正,
         <br>"LSMR法における条件数を用いた反復の打ち切りと機械学習について",
         <br>第20回計算数学研究会,
-        <br><a href="https://www-is.amp.i.kyoto-u.ac.jp/lab/cmc/" target="_blank">数理解析分野研究室</a>, 京都大学,
+        <br><a href="https://www-is.amp.i.kyoto-u.ac.jp/lab/cmc/" target="_blank">
+        京都大学大学院 情報学研究科 数理解析分野研究室</a>,
         <br>オンライン, 日本,
         <br>2022.8.25
       </li>
@@ -88,7 +106,7 @@ document.write(`
       <li>
         Itsuki Kuboi, Rika Tanaka, Shin-ya Ozawa, Yohsuke Hosoda, Masami Takata*, Kinji Kimura and Yoshimasa Nakamura,
         <br><a href="https://www.american-cse.org/static/CSCE22-book-abstracts-printing.pdf" target="_blank">
-          "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
+              "On a Fast Computation of the Condition L-curve in the LSMR Method for Solving Ill-conditioned Problems using the DQDS Method"</a>,
         <br>The 28th International Conference on Parallel & Distributed Processing Techniques & Applications,
         <!-- <br> - The 2022 World Congress in Computer Science, Computer Engineering, and Applied Computing, -->
         <br>American Council on Science and Education,
@@ -100,7 +118,7 @@ document.write(`
       <li>
         久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美*, 木村欣司, 中村佳正,
         <br><a href="https://www.ipsj.or.jp/kenkyukai/event/mps139.html" target="_blank">
-          "不良条件問題を解くためのLSMR法におけるDQDS法を用いたcondition L-curveの高速計算について"</a>,
+              "不良条件問題を解くためのLSMR法におけるDQDS法を用いたcondition L-curveの高速計算について"</a>,
         <br>MPS139,
         <br>MPS研究会, 情報処理学会,
         <br>Luxor Hotel, Las Vegas, USA,
@@ -110,7 +128,8 @@ document.write(`
       <br>
       <li>
         木村欣司*, 久保井五貴, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 
-        <br><a href="http://air.s.kanazawa-u.ac.jp/~ohara/risacon21.html" target="_blank">"離散戸田分子方程式と逆問題"</a>,
+        <br><a href="http://air.s.kanazawa-u.ac.jp/~ohara/risacon21.html" target="_blank">
+              "離散戸田分子方程式と逆問題"</a>,
         <br>Risa/Asir Conference 2021,
         <br>金沢大学理工研究域数物科学系計算数理講座, 石川, 日本,
         <br>オンライン, 日本,
@@ -120,7 +139,8 @@ document.write(`
       <br>
       <li>
         久保井五貴*, 田中利佳, 小澤伸也, 細田陽介, 高田雅美, 木村欣司,
-        <br><a href="http://union2021.jsiam.org/program#4863" target="_blank">"デコンボリューション問題におけるcondition L-curveの検討"</a>,
+        <br><a href="http://union2021.jsiam.org/program#4863" target="_blank">
+              "デコンボリューション問題におけるcondition L-curveの検討"</a>,
         <br>第17回研究部会連合発表会,
         <br>日本応用数理学会,
         <br>オンライン, 日本,
@@ -130,7 +150,8 @@ document.write(`
       <br>
       <li>
         久保井五貴*, 細田陽介, 小澤伸也,
-        <br><a href="http://2019.jhes.jp/files/program.pdf" target="_blank">"定常反復を用いたデコンボリューション問題の数値計算について"</a>,
+        <br><a href="http://2019.jhes.jp/files/program.pdf" target="_blank">
+              "定常反復を用いたデコンボリューション問題の数値計算について"</a>,
         <br>2019年度電気・情報関係学会北陸支部連合大会,
         <br>共催[IEEJ, IEICE, ITE, JSMBE, IPSJ, SICE, ASJ, IEIEJ各北陸支部, IEEE名古屋支部],
         <br>石川工業高等専門学校, 石川, 日本,
@@ -139,6 +160,7 @@ document.write(`
     </ol>
   </li>
 
+  
   <br>
   <li><b>記法</b>
     <ol reversed>
