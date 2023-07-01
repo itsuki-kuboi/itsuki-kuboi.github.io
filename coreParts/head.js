815b8a658cc94f77a98854c1888386ff2d8fcca8
Itsuki KUBOI <emacs75e@gmail.com>
2023-07-02 00:40:36 +0900

    mod css

--- coreParts/head.js
+++ coreParts/head.js
@@ -2,15 +2,27 @@
 /*https://www.site-convert.com/archives/1528#outline__1*/
 /*https://www.webdesignleaves.com/wp/htmlcss/450/*/
 
+/* 2023.7 : cssの分割、resetCSSの導入 */
+/* https://diytech.website/preparing-for-responsive/ */
+
 /*charsetは各HTMLで宣言しないと反映されない*/
 
 document.write(`
 <meta http-equiv="Content-Type" content="text/html">
 <meta http-equiv="Content-Style-Type" content="text/css">
+
+<!-- viewportの設定 -->
 <!--<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">-->
 <meta name="viewport" content="width=device-width, initial-scale=0.6, minimum-scale=0.6, user-scalable=yes">
+<!--<link rel="stylesheet" href="./css/destyle.css"> --> <!-- リセットCSS -->
 
-<link rel="stylesheet" href="./css/cv.css">
+
+<!-- リセットCSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
+<!-- stylesheetCSS -->
+<link rel="stylesheet" href="./css/stylesheet.css">
+<!-- responsiveCSS -->
+<link rel="stylesheet" href="./css/responsive.css">
 
 <title>Itsuki KUBOI's  Website</title>
 
