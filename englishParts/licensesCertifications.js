e10f52531e3a16ddb09b27cf8ffbfcd4012d0f77
Itsuki KUBOI <emacs75e@gmail.com>
2025-12-03 12:29:22 +0900

    add license ama3

--- englishParts/licensesCertifications.js
+++ englishParts/licensesCertifications.js
@@ -86,23 +86,23 @@ document.write(`
     -->
 
 
-    <!-- <li>2026.5.xx - Present
+    <!-- <li>2026.9.xx - Present
     <br><b>Amateur First-Class Radio Operator</b>(Application in progress),
     <br>MIC, Japan
     </li>
     -->
-    <!-- <li>2026.5.xx - Present
+    <!-- <li>2026.9.xx - Present
     <br><b>Amateur Second-Class Radio Operator</b>(Application in progress),
     <br>MIC, Japan
     </li>
     -->
     
-    <li>2025.11.xx - Present
-    <br><b>Amateur Third-Class Radio Operator</b>(Application in progress),
+    <li>2025.12.1 - Present
+    <br><b>Amateur Third-Class Radio Operator</b>, <!-- (Application in progress) -->
     <br>MIC, Japan
     </li>
     <li>2025.11.5 - Present
-    <br><b>Amateur Fourth-Class Radio Operator</b>,
+    <br><b>Amateur Fourth-Class Radio Operator</b>, <!-- (Application in progress) -->
     <br>MIC, Japan
     </li>
     
